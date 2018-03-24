615db81b000aeb044dcbe0b4b2e63c8c4da801bf
Lyndsay Fearnall <lyndsayfearnall@Lyndsays-MacBook-Pro-2.local>
2018-03-24 15:55:17 -0400

    styled admin index page

---
+++ includes/adminNav.php
@@ -0,0 +1,19 @@
+<nav class = "navbar navbar-expand-lg" id="adminNav">
+  <a class="navbar-brand" href="adminIndex.php">Moviez R Cool </a>
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"><i class="material-icons" id="hamburger">menu</i></span>
+  </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item"><a class="nav-link" href="#">Link 1</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Link 2</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Link 3</a></li>
+
+      </ul>
+      <ul class="navbar-nav navbar-right">
+        <li class="nav-item"><a class="nav-link" href = "phpscripts/caller.php?caller_id=logout">Logout</a></li>
+        <!-- <li class="nav-item"><a class="nav-link" href = "phpscripts/caller.php?caller_id=logout">Logout</a></li> -->
+      </ul>
+    </div>
+</nav>
---
+++ includes/adminSidebar.php
@@ -0,0 +1,16 @@
+  <div class="col-2 sidebar-sticky" id="sideBar">
+    <ul class="nav flex-column mt-3">
+      <li class="nav-item">
+        <a class="nav-link" href="adminIndex.php"><i class="material-icons icon">home</i>Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href ="#"><i class="material-icons">local_movies</i>Add Movie</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="material-icons">mode_edit</i>Edit Movie</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="admin_manageUsers.php"><i class="material-icons">person_add</i>Manage Admin</a>
+      </li>
+    </ul>
+  </div>
