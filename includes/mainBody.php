46e04719d6a15cd616fa14e65406bc3f4e11140a
Lyndsay Fearnall <lyndsayfearnall@Lyndsays-MacBook-Pro-2.local>
2018-03-23 21:56:47 -0400

    footer + body styling

---
+++ includes/mainBody.php
@@ -0,0 +1,5 @@
+<div class="container">
+  <div class = "row">
+    <p class="col">BODY COPY HERE</p>
+  </div>
+</div>
---
+++ includes/mainFooter.php
@@ -0,0 +1,7 @@
+<footer class="navbar" id="mainFooter">
+  <div class="row">
+    <div class="col-12">
+      <p class="text-center" id="cp"> &copy <?php echo date('Y');?></p>
+    </div>
+  </div>
+</footer>
